2e6557b8d34f5f22244b79ff53f2ca784909ea66
judibec <juanchopolo6954@gmail.com>
2022-03-02 21:58:04 -0500

    Punto 4, faltan los ultimos numerales

--- Servlet/src/main/java/edu/eci/cvds/calculadora/Calculadora.java
+++ Servlet/src/main/java/edu/eci/cvds/calculadora/Calculadora.java
@@ -3,16 +3,17 @@ package edu.eci.cvds.calculadora;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import java.util.ArrayList;
 
 @ManagedBean(name = "calculadoraBean")
-@ApplicationScoped
-
+//@ApplicationScoped
+@SessionScoped
 
 public class Calculadora {
 
     private double resModa;
-    private double resMediana;
+    private double resPromedio;
     private double resVarianza;
     private double resDesvEstandar;
     private int cantNum;
@@ -20,6 +21,77 @@ public class Calculadora {
 
 
     public Calculadora(){
+    }
+
+    private double[] stringToDouble(){
+        String[] values = numeros.split(";");
+        double[] newValues = new double[values.length];
+        for (int i = 0; i < values.length; i++){
+            newValues[i] = Double.parseDouble(values[i]);
+            cantNum ++;
+        }
+        return newValues;
+    }
+
+    public double calculateMean(){
+        double[] values = stringToDouble();
+        double sum = 0;
+        for (double value : values) {
+            sum += value;
+        }
+        resPromedio = sum / values.length;
+        return resPromedio;
+    }
+
+    public double calculateStandardDeviation(){
+        resDesvEstandar = Math.sqrt(calculateVariance());
+        return resDesvEstandar;
+    }
+
+    private double PcalculateMean(){
+        double[] values = stringToDouble();
+        double sum = 0;
+        for (double value : values) {
+            sum += value;
+        }
+        return sum / values.length;
+    }
+
+    public double calculateVariance(){
+        double[] values = stringToDouble();
+        double variance = 0;
+        for (double value : values) {
+            variance += (value - PcalculateMean()) * (value - PcalculateMean());
+        }
+        resVarianza = variance / values.length;
+        return resVarianza;
+    }
+
+    public double calculateMode(){
+        int max = 0;
+        double[] values = stringToDouble();
+        for (double i : values) {
+            int cont = 0;
+            for (double j : values) {
+                if (i == j) {
+                    cont++;
+                }
+            }
+            if(cont > max){
+                max = cont;
+                resModa = i;
+            }
+        }
+        return resModa;
+    }
+
+    public void restart(){
+        numeros = "";
+        resPromedio = 0;
+        resVarianza = 0;
+        resDesvEstandar = 0;
+        resModa = 0;
+        cantNum = 0;
     }
 
     public double getResModa() {
@@ -30,12 +102,12 @@ public class Calculadora {
         this.resModa = resModa;
     }
 
-    public double getResMediana() {
-        return resMediana;
+    public double getResPromedio() {
+        return resPromedio;
     }
 
-    public void setResMediana(double resMediana) {
-        this.resMediana = resMediana;
+    public void setResPromedio(double resPromedio) {
+        this.resPromedio = resPromedio;
     }
 
     public double getResVarianza() {
@@ -72,3 +144,4 @@ public class Calculadora {
 
 
 }
+
